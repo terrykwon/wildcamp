4be927afb79099a5407fb9dcb26631bd43145a6f
Terry Kwon <terrykkwon@gmail.com>
2017-06-14 00:39:58 +0900

    Remove unnecessary file upload checks

--- add-post.php
+++ add-post.php
@@ -1,4 +1,7 @@
+
+
 <?php
+// -------------------------------------------------- Upload
     // Check login
     // session_start();
     // if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
@@ -14,33 +17,36 @@
     $target_file = $target_dir . basename($_FILES["photo"]["name"]);
     $uploadOk = 1;
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
     // Check if image file is a actual image or fake image
-    if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["photo"]["tmp_name"]);
-        if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
-        }
-    }
+    // if(isset($_POST["submit"])) {
+    //     $check = getimagesize($_FILES["photo"]["tmp_name"]);
+    //     if($check !== false) {
+    //         echo "File is an image - " . $check["mime"] . ".";
+    //         $uploadOk = 1;
+    //     } else {
+    //         echo "File is not an image.";
+    //         $uploadOk = 0;
+    //     }
+    // }
+
     // Check if file already exists
     if (file_exists($target_file)) {
         echo "Sorry, file already exists.";
         $uploadOk = 0;
     }
     // Check file size
-    if ($_FILES["photo"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
-        $uploadOk = 0;
-    }
+    // if ($_FILES["photo"]["size"] > 500000000) {
+    //     echo "Sorry, your file is too large.";
+    //     $uploadOk = 0;
+    // }
+
     // Allow certain file formats
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-    }
+    // if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    // && $imageFileType != "gif" ) {
+    //     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    //     $uploadOk = 0;
+    // }
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
         echo "Sorry, your file was not uploaded.";
@@ -52,6 +58,7 @@
             echo "Sorry, there was an error uploading your file.";
         }
     }
+// --------------------------------------------------------------------
 
     $description = $_POST["description"];
     $species = $_POST["species"];
@@ -69,9 +76,7 @@
     $loc = explode(',', $_POST["location"]);
     $lat = $loc[0];
     $lon = $loc[1];
-?>
 
-<?php
     $servername = "www.moonpark.biz";
     $username = "wildcamp";
     $password = "Wildcamp1234!";
